5099c23d5f33e397ef253bb65ae36d87e0451c4d
mrquaternion <mathiaslarochelle1@gmail.com>
2026-04-22 12:43:32 -0400

    new changes regarding multi-fluxes handling + train gapfill sep

---
+++ matlab/Micromet/train_ML_gapfill.m
@@ -0,0 +1,22 @@
+function train_ML_gapfill(Years,Sites)
+% funtion to call full run, including training of ML gapfilling pipeline
+% if a model has already been trained, this can be skipped and gapfilling can be run using fr_automated_cleaning(Years,Sites,9)
+
+numOfYears = length(Years);
+numOfSites = length(Sites);
+
+for cntSites = 1:numOfSites
+    siteID = upper(char(Sites(cntSites)));
+    
+    for cntYears = 1:numOfYears
+        yy = Years(cntYears);
+        yy_str = num2str(yy(1));
+
+        %------------------------------------------------------------------
+        % 9th stage is the methane-gapfill-ml python pipeline
+        %------------------------------------------------------------------
+        disp(['============== Running full ML gapfilling pipeline including training: ' siteID ' ' yy_str ' ==============']);
+        runThirdStageCleaningMethaneGapfillML_test(yy, siteID, 1);
+        fprintf('============== End of cleaning stage 9 =============\n'); 
+    end
+end
